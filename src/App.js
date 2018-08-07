0433f1b58c6cd568dda68b848d23827ad4910115
Octowl <alsaff1987@gmail.com>
2018-08-07 16:00:58 +0300

    Add a searchable booklist component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Sidebar from "./Sidebar";
 import Loading from "./Loading";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import BookList from "./BookList";
 
 const instance = axios.create({
   baseURL: "https://the-index-api.herokuapp.com"
@@ -17,12 +18,17 @@ class App extends Component {
     super(props);
     this.state = {
       authors: [],
-      authorsLoading: true
+      authorsLoading: true,
+      booksLoading: true
     };
   }
 
   fetchAllAuthors() {
     return instance.get("/api/authors/").then(res => res.data);
+  }
+
+  fetchAllBooks() {
+    return instance.get("/api/books/").then(res => res.data);
   }
 
   componentDidMount() {
@@ -33,6 +39,10 @@ class App extends Component {
           authorsLoading: false
         })
       )
+      .catch(err => console.error(err));
+
+    this.fetchAllBooks()
+      .then(books => this.setState({ books: books, booksLoading: false }))
       .catch(err => console.error(err));
   }
 
@@ -49,6 +59,10 @@ class App extends Component {
             render={props => (
               <AuthorsList {...props} authors={this.state.authors} />
             )}
+          />
+          <Route
+            path="/books/"
+            render={props => <BookList {...props} books={this.state.books} />}
           />
         </Switch>
       );
